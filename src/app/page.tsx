1c8f81b1de467c5c1179470502346b5e58cf27ed
Pedro Souza <pedro.souza4237@gmail.com>
2024-03-27 17:33:43 -0300

    Created the project using tailwind and nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="p-4 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl text-diego">Hello Tailwind</h1>
+      <h2>Hello Tailwind</h2>
+      <p>Hello Tailwind</p>
+    </div>
+  );
+}
