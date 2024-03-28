b0f24409b2a4df3f0aab61cc0f0c4da286bada2d
Pedro Souza <pedro.souza4237@gmail.com>
2024-03-28 12:27:05 -0300

    Created the layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center bg-slate-50  p-4 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100">
-      <div className="max-w-2xl">
-        <h1 className="text-5xl font-bold">
-          Rapidly build modern websites without ever leaving your HTML.
-        </h1>
-        <h2 className="mt-4 dark:text-slate-400">
-          A utility-first CSS framework packed with classes like flex, pt-4,
-          text-center and rotate-90 that can be composed to build any design,
-          directly in your markup.
-        </h2>
-        <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-semibold text-white enabled:hover:bg-sky-700 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-sky-400 dark:text-slate-950">
-          Sign in
-        </button>
-      </div>
+    <div>
+      <h1>Home</h1>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
       colors: {
-        diego: '#8257e6'
-      }
+        diego: '#8257e6',
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
