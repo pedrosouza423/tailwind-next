0f7624caf3c8705dc642ff79ec5dacd1d635ef0b
Pedro Souza <pedro.souza4237@gmail.com>
2024-03-29 19:24:22 -0300

    Added a UsedSpaceWidget component in the sidebar

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Logo } from './logo'
-import { Search } from 'lucide-react'
-import { MainNavigation } from './MainNavigation'
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  Settings,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export const Sidebar = () => {
   return (
-    <aside className="mx-1 space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="mx-1 flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
       <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
@@ -15,7 +26,22 @@ export const Sidebar = () => {
         />
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto">
+        <nav className="space-y-0.5">
+          <NavItem title="Suport" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Settings} />
+        </nav>
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
