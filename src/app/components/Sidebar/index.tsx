f9d165fedb3976031636fd19be1615e6060263a3
Pedro Souza <pedro.souza4237@gmail.com>
2024-04-01 13:22:42 -0300

    Added composition pattern in the Input

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -14,18 +14,18 @@ import {
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import * as Input from '../Input'
 
 export const Sidebar = () => {
   return (
     <aside className="mx-1 flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 focus:outline-none"
-          placeholder="Search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
