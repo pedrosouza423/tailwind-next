a54fbab93d71d251a8e6282743ddef74a3d5ea49
Pedro Souza <pedro.souza4237@gmail.com>
2024-04-05 22:52:34 -0300

    Context and Image Preview

--- src/app/components/Form/FileInput/Control.tsx
+++ src/app/components/Form/FileInput/Control.tsx
@@ -1,7 +1,27 @@
-import { ComponentProps } from 'react'
+'use client'
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
 
 type ControlProps = ComponentProps<'input'>
 
 export const Control = (props: ControlProps) => {
-  return <input {...props} type="file" className="sr-only" id="photo" />
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+    onFilesSelected(files)
+  }
+  return (
+    <input
+      {...props}
+      type="file"
+      className="sr-only"
+      onChange={handleFilesSelected}
+      id={id}
+    />
+  )
 }
